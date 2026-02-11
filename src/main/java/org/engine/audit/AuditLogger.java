e219e0c762984ee13d987eeb1286bbc1511d8105
Kauan Oliveira <kauandias747474@gmail.com>
2026-02-11 07:58:34 -0300

    Update AuditLogger.java

--- src/main/java/org/engine/audit/AuditLogger.java
+++ src/main/java/org/engine/audit/AuditLogger.java
@@ -1,1 +1,30 @@
+ import java.time.LocalDateTime;
+
+ class ConsoleRepository implements AuditRepository {
+     @Override
+     public void salvar(AuditEntry entry) {
+         System.out.println("--- NOVO LOG DE AUDITORIA ---");
+         System.out.println("Data: " + entry.data());
+         System.out.println("Nível: " + entry.status() + " (Grau: " + entry.status().getNivel() + ")");
+         System.out.println("Mensagem: " + entry.mensagem());
+         System.out.println("------------------------------");
+     }
+ }
+     public class Main {
+
+
+       static void main() {
+
+         AuditRepository repo = new ConsoleRepository();
+
+
+         AuditEntry log = new AuditEntry(
+                 "Sistema iniciado com sucesso",
+                 Status.INFO,
+                 LocalDateTime.now()
+         );
+
+         repo.salvar(log);
+     }
+     }
 
