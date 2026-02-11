0a4d92425812156ee02ed1f1e4c7df6df489d5c1
Kauan Oliveira <kauandias747474@gmail.com>
2026-02-11 07:57:57 -0300

    Update Status.java

--- src/main/java/org/engine/audit/Status.java
+++ src/main/java/org/engine/audit/Status.java
@@ -1,1 +1,16 @@
+public enum Status {
+    INFO(1),
+    ALERTA(2),
+    CRITICO(3);
+
+    private final int nivel;
+
+    Status(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+}
 
